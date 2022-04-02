110a9193a2c264db94df88d09b05791b5ade98e3
Rudra <100891455+Leopardagamingyt@users.noreply.github.com>
2022-04-03 04:04:06 +0530

    Update non-auth-user.py

--- bot/plugins/non-auth-user.py
+++ bot/plugins/non-auth-user.py
@@ -1,18 +1,1 @@
-import logging
 
-from pyrogram import filters as Filters
-from pyrogram.types import Message
-
-from ..utubebot import UtubeBot
-from ..config import Config
-
-
-log = logging.getLogger(__name__)
-
-
-@UtubeBot.on_message(
-    Filters.private & Filters.incoming & ~Filters.user(Config.AUTH_USERS)
-)
-async def _non_auth_usr_msg(c: UtubeBot, m: Message):
-    await m.delete(false)
-    
