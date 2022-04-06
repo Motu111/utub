067236927429cd9c53f39dcb8ea147ff24440f1f
Rudra <100891455+Leopardagamingyt@users.noreply.github.com>
2022-04-06 16:10:14 +0530

    Update translations.py

--- bot/translations.py
+++ bot/translations.py
@@ -9,9 +9,9 @@ class Messages:
         ".",
         "Hello boss.\n\nHii. Every video uploaded by you is processed by YT, "
         "if you upload copyright video then you can be blocked "
-        "so be alert, and you will not be able to publish the video.\n\nRead through all the pages to know how I work.",
-        "**Lets learn how I work.**\n\n**Step 1:** __You authorise me to upload to your youtube channel.More about "
-        "this in comming pages.__\n\n**Step 2:** __You forward any Telegram video to me.__\n\n**Step 3:** __You reply "
+        "so be alert, and you will not be able to publish the video.\n\nRead all pages carefully.",
+        "**Here i will tell how i work.**\n\n**Step 1:** __Authorise me to upload to your youtube channel "
+        "It is so easy.__\n\n**Step 2:** __You forward any Telegram video to me.__\n\n**Step 3:** __You reply "
         "__/upload __to the forwarded video file.You can also specify some title in the upload command, but its "
         "optional though.Title will follow the __`/upload`.__If no title is given, filename will be used as title.__"
         "\n\n**Step 4:** __I remotely download the file and uploads to your Youtube channel.__\n\n**Step 5:** __I "
